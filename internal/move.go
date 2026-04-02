7087fa0510d68d9bab32f3c43d81f909290a03b6
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-02 20:37:20 +0200

    feat: Move helper

---
+++ internal/move.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/charmbracelet/log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func Move(paths []string, recursive, force, verbose bool) error {
+	err := EnsureDirs()
+	if err != nil {
+		return err
+	}
+
+	entries, _ := LoadMetadata()
+
+	for _, p := range paths {
+		info, err := os.Stat(p)
+		if err != nil {
+			if force {
+				continue
+			}
+			return err
+		}
+
+		if info.IsDir() && !recursive {
+			return fmt.Errorf("can't trash %s: is a directory. Try with -r", p)
+		}
+
+		id := fmt.Sprintf("%d", time.Now().UnixNano())
+		name := filepath.Base(p)
+		stored := id + "_" + name
+		dest := filepath.Join(FilesDir(), stored)
+
+		err = os.Rename(p, dest)
+		if err != nil {
+			return err
+		}
+
+		abs, _ := filepath.Abs(p)
+
+		entries = append(entries, Entry{
+			ID:           id,
+			OriginalPath: abs,
+			StoredName:   stored,
+			DeletedAt:    time.Now(),
+			IsDir:        info.IsDir(),
+		})
+
+		if verbose {
+			log.Info("trashed %s", p)
+		}
+	}
+
+	return SaveMetadata(entries)
+}
