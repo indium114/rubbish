aebd66393fe6eadc3772cf6f2a248a9f1debdba0
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-06 21:26:13 +0200

    chore: bump version in flake

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
         packages.rubbish = pkgs.buildGoModule {
           pname = "rubbish";
-          version = "2026.03.01-a";
+          version = "2026.04.06-a";
 
           src = self;
 
