9dd2b963fa52548cc9db9234afd277a8eace9ed4
indium114 <156162907+indium114@users.noreply.github.com>
2026-09-09 19:14:59 +0200

    feat: basic cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use clap::Parser;
+use std::{path::PathBuf, process};
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser)]
+#[command(name = "rubbish")]
+#[command(version = &VERSION)]
+#[command(about = "A CLI file trash tool", long_about = None)]
+struct Cli {
+    #[arg(short = 'r', long = "recursive")]
+    recursive: bool,
+
+    #[arg(short = 'f', long = "force")]
+    force: bool,
+
+    #[arg(short = 'v', long = "verbose")]
+    verbose: bool,
+
+    #[arg(short = 'p', long = "permanent")]
+    permanent: bool,
+
+    #[arg(long = "list")]
+    list: bool,
+
+    #[arg(long = "restore", value_name = "id")]
+    restore: Option<String>,
+
+    #[arg(long = "delete", value_name = "id")]
+    delete: Option<String>,
+
+    #[arg(long = "clear")]
+    clear: bool,
+
+    #[arg(trailing_var_arg = true)]
+    files: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let _ = if cli.list {
+        todo!()
+    } else if let Some(id) = &cli.restore {
+        todo!()
+    } else if let Some(id) = &cli.delete {
+        todo!()
+    } else if cli.clear {
+        todo!()
+    } else if cli.files.is_empty() {
+        eprintln!("No files provided");
+        process::exit(1);
+    } else {
+        todo!(
+            /*
+            &cli.files,
+            cli.recursive,
+            cli.force,
+            cli.verbose,
+            cli.permanent,
+            */
+        )
+    };
 }
